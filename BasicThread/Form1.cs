73cd8a0becef9fd04237a521ba52dbc67f708895
mercadillo.260317 <mercadillo.260317@LAB-A-PC10>
2023-10-20 18:59:37 +0800

    Add project files.

---
+++ BasicThread/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicThread
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Thread ThreadA;
+        private Thread ThreadB;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart start = new ThreadStart(MyThreadClass.Thread1);
+            Console.WriteLine(label1.Text.ToString());
+
+            ThreadA = new Thread(start);
+            ThreadA.Name = "Thread A";
+            ThreadA.Start();
+
+            ThreadB = new Thread(start);
+            ThreadB.Name = "Thread B";
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            label1.Text = "- End of Thread -";
+            Console.WriteLine(label1.Text.ToString());
+
+        }
+    }
+}
---
+++ BasicThread/MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1() 
+        {
+            for (int loopcount = 1; loopcount <= 5; loopcount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopcount);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
